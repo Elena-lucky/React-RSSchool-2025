437be43be1689215648bce9d675f294dcb556511
Elena-lucky <elena.ivanovada@gmail.com>
2025-01-26 22:54:01 +0300

    feat: add spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Search from './components/search/Search';
 import Result from './components/result/Result';
+import Spinner from './components/spinner/Spinner';
 import { fetchSearchResults } from './services/Api';
 import './App.css';
 
@@ -9,6 +10,7 @@ class App extends React.Component {
     query: '',
     result: null,
     error: null,
+    isLoading: false,
   };
 
   componentDidMount() {
@@ -16,22 +18,23 @@ class App extends React.Component {
   }
 
   handleSearch = async (query: string) => {
-    this.setState({ query, error: null });
+    this.setState({ query, error: null, isLoading: true });
 
     try {
       const result = await fetchSearchResults(query);
-      this.setState({ result });
+      this.setState({ result, isLoading: false });
     } catch {
-      this.setState({ error: 'Please try again.' });
+      this.setState({ error: 'Please try again.', isLoading: false });
     }
   };
 
   render() {
-    const { result, error } = this.state;
+    const { result, error, isLoading } = this.state;
 
     return (
       <div>
         <Search onSearchClick={this.handleSearch} />
+        {isLoading && <Spinner />} {}
         {error && <p className="error">{error}</p>}
         {result && <Result data={result} />}
       </div>
