7df57960dfa1844a9ba3365fd45ce100ef4b7ba4
Elena-lucky <elena.ivanovada@gmail.com>
2025-02-06 09:18:15 +0300

    fix: fix custom hook

--- src/hooks/useSearchQuery.tsx
+++ src/hooks/useSearchQuery.tsx
@@ -1,9 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const useSearchQuery = (): [string, (query: string) => void] => {
-  const [query, setQuery] = useState(
-    () => localStorage.getItem('searchQuery') || ''
-  );
+  const [query, setQuery] = useState(localStorage.getItem('searchQuery') || '');
 
   useEffect(() => {
     localStorage.setItem('searchQuery', query);
